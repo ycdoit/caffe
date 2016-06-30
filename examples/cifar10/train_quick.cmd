d282bb37c98760df5aa4d7610b9ecf0667a4ed31
Junjie Li <junli@microsoft.com>
2016-06-30 16:23:51 +0800

    add windows run scripts

---
+++ examples/cifar10/train_quick.cmd
@@ -0,0 +1,2 @@
+"Build/x64/Debug/caffe.exe" train --solver=examples/cifar10/cifar10_quick_solver.prototxt
+"Build/x64/Debug/caffe.exe" train --solver=examples/"cifar10/cifar10_quick_solver_lr1.prototxt --snapshot=examples/cifar10/cifar10_quick_iter_4000.solverstate.h5
