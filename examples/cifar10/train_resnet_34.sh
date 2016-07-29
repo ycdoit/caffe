22a1fe03c8438a73d2d0a4a1d0cc35e750eb807c
Junjie Li <junli@microsoft.com>
2016-07-29 13:53:59 +0800

    update resnet 34 proto

--- examples/cifar10/train_resnet_34.sh
+++ examples/cifar10/train_resnet_34.sh
@@ -8,9 +8,9 @@ $TOOLS/caffe train \
 # reduce learning rate by factor of 10
 $TOOLS/caffe train \
     --solver=examples/cifar10/cifar10_resnet_34_solver_lr1.prototxt \
-    --snapshot=examples/cifar10/cifar10_resnet_34_iter_16000.solverstate.h5
+    --snapshot=examples/cifar10/cifar10_resnet_34_iter_32000.solverstate.h5
 
 # reduce learning rate by factor of 10
 $TOOLS/caffe train \
     --solver=examples/cifar10/cifar10_resnet_34_solver_lr2.prototxt \
-    --snapshot=examples/cifar10/cifar10_resnet_34_iter_24000.solverstate.h5
+    --snapshot=examples/cifar10/cifar10_resnet_34_iter_48000.solverstate.h5
