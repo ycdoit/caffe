bb45596de8c6afc1a1347154c52f298fcac8aca0
Junjie Li <junli@microsoft.com>
2016-07-30 19:51:46 +0800

    fix script

--- examples/cifar10/train_full_4P4G1M.sh
+++ examples/cifar10/train_full_4P4G1M.sh
@@ -2,6 +2,6 @@
 
 TOOLS=./build/tools
 
-$TOOLS/caffe asgd_train \
+mpiexec -n 4 $TOOLS/caffe asgd_train \
     --solver=examples/cifar10/cifar10_full_solver_1p4G0M.prototxt \
     --gpus_per_process=1
