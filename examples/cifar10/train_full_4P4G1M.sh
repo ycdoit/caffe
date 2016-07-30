b60ad45aeb1284515e4dc496b64f2198fcffebfa
Junjie Li <junli@microsoft.com>
2016-07-30 20:07:52 +0800

    fix bug

--- examples/cifar10/train_full_4P4G1M.sh
+++ examples/cifar10/train_full_4P4G1M.sh
@@ -3,5 +3,5 @@
 TOOLS=./build/tools
 
 mpiexec -n 4 $TOOLS/caffe asgd_train \
-    --solver=examples/cifar10/cifar10_full_solver_1p4G0M.prototxt \
+    --solver=examples/cifar10/cifar10_full_solver_4p4G1M.prototxt \
     --gpus_per_process=1
